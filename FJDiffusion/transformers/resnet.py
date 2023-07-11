e29b4556558757a7a42ddbbacb3d90e0d6a3de87
erfanzar <erfanzare82@yahoo.com>
2023-07-11 23:31:32 +0330

    adding `FlaxDownEncoderBlock2D`

--- FJDiffusion/transformers/resnet.py
+++ FJDiffusion/transformers/resnet.py
@@ -74,3 +74,60 @@ class FlaxResnetBlock2D(nn.Module):
         if self.cs is not None:
             residual = self.cs(residual)
         return hidden_state + residual
+
+
+class FlaxResnetBlock2DNTime(nn.Module):
+    in_c: int
+    out_c: int = None
+    use_shortcut: bool = None
+    dropout_rate: float = 0.0
+    epsilon: float = 1e-5
+    dtype: jnp.dtype = jnp.float32
+    param_dtype: jnp.dtype = jnp.float32
+    precision: typing.Optional[typing.Union[None, jax.lax.Precision]] = None
+
+    def setup(self) -> None:
+        out_c = self.out_c or self.in_c
+        self.c1 = nn.Conv(
+            features=self.in_c,
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            padding=((1, 1), (1, 1)),
+            dtype=self.dtype,
+            param_dtype=self.param_dtype,
+            precision=self.precision
+        )
+        self.norm1 = nn.GroupNorm(32, epsilon=self.epsilon)
+
+        self.c2 = nn.Conv(
+            features=out_c,
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            padding=((1, 1), (1, 1)),
+            dtype=self.dtype,
+            param_dtype=self.param_dtype,
+            precision=self.precision
+        )
+        self.norm2 = nn.GroupNorm(32, epsilon=self.epsilon)
+
+        self.drop = nn.Dropout(self.dropout_rate)
+
+        cut = self.in_c != out_c if self.use_shortcut is None else self.use_shortcut
+        if cut:
+            self.cs = nn.Conv(
+                out_c,
+                kernel_size=(1, 1),
+                padding="VALID",
+                strides=(1, 1),
+                dtype=self.dtype,
+                param_dtype=self.param_dtype,
+                precision=self.precision
+            )
+
+    def __call__(self, hidden_state, deterministic=False):
+        residual = hidden_state
+        hidden_state = self.c1(nn.swish(self.norm1(hidden_state)))
+        hidden_state = self.c2(self.drop(nn.swish(self.norm2(hidden_state)), deterministic=deterministic))
+        if self.cs is not None:
+            residual = self.cs(residual)
+        return hidden_state + residual
---
+++ FJDiffusion/transformers/vae.py
@@ -0,0 +1,58 @@
+from jax import numpy as jnp
+import jax
+from flax import linen as nn
+from FJDiffusion.transformers.resnet import FlaxResnetBlock2D, FlaxResnetBlock2DNTime
+from FJDiffusion.moonwalker.utils import Downsample, Upsample
+from FJDiffusion.transformers.utils import get_gradient_checkpointing_policy
+import typing
+
+
+class FlaxDownEncoderBlock2D(nn.Module):
+    in_channels: int
+    out_channels: int
+    dropout_rate: float = 0.0
+    add_down_sampler: bool = True
+    epsilon: float = 1e-5
+    num_hidden_layers: int = 1
+    gradient_checkpointing: str = 'nothing_saveable'
+    dtype: jnp.dtype = jnp.float32
+    param_dtype: jnp.dtype = jnp.float32
+    precision: typing.Optional[typing.Union[None, jax.lax.Precision]] = None
+
+    def setup(self) -> None:
+        resnet = []
+
+        block_class = nn.remat(FlaxResnetBlock2DNTime, policy=get_gradient_checkpointing_policy(
+            self.gradient_checkpointing)) if self.gradient_checkpointing != '' else FlaxResnetBlock2DNTime
+        for i in range(self.num_hidden_layers):
+            in_channel = self.in_channels if i == 0 else self.out_channels
+            block = block_class(
+                in_c=in_channel,
+                out_c=self.out_channels,
+                use_shortcut=None,
+                dropout_rate=self.dropout_rate,
+                epsilon=self.epsilon,
+                dtype=self.dtype,
+                param_dtype=self.param_dtype,
+                precision=self.precision
+            )
+            resnet.append(block)
+
+        self.resnets = resnet
+        if self.add_down_sampler:
+            self.down_sampler = Downsample(
+                in_channel=self.out_channels,
+                dtype=self.dtype,
+                param_dtype=self.param_dtype,
+                precision=self.precision
+            )
+
+    def __call__(self, hidden_state, time, deterministic: bool = True):
+        for block in self.resnets:
+            hidden_state = block(
+                hidden_state=hidden_state,
+                deterministic=deterministic
+            )
+        if self.add_down_sampler:
+            hidden_state = self.down_sampler(hidden_state)
+        return hidden_state
