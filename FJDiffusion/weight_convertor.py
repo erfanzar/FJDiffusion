b35e8ccb8f3b3b10a4e4cfb6d7ef51e039198ee7
erfanzar <erfanzare82@yahoo.com>
2023-07-15 15:28:48 +0330

    `fixing naming issue`

--- FJDiffusion/weight_convertor.py
+++ FJDiffusion/weight_convertor.py
@@ -3,12 +3,16 @@ def convert_vae_weights(vae_params_flatten, debug: bool = False):
     for prm, v in vae_params_flatten.items():
         org_prm = prm
         if prm[0] == 'decoder':
+
             if prm[1] == 'mid_block':
+
                 if prm[2].startswith('resnets_'):
                     if prm[3] == 'conv1':
                         prm = prm[:3] + ('c1',) + prm[4:]
                     if prm[3] == 'conv2':
                         prm = prm[:3] + ('c2',) + prm[4:]
+                    if prm[3] == 'conv_shortcut':
+                        prm = prm[:3] + ('cs',) + prm[4:]
                 prm = ('decoder', 'bottle_neck') + prm[2:]
 
             if prm[1].startswith('up_blocks_'):
@@ -18,8 +22,11 @@ def convert_vae_weights(vae_params_flatten, debug: bool = False):
                         prm = prm[:3] + ('c1',) + prm[4:]
                     if prm[3] == 'conv2':
                         prm = prm[:3] + ('c2',) + prm[4:]
+                    if prm[3] == 'conv_shortcut':
+                        prm = prm[:3] + ('cs',) + prm[4:]
                 prm = ('decoder', f'decoders_{num}') + prm[2:]
-
+            if prm[1] == 'conv_norm_out':
+                prm = prm[0:1] + ('out_norm',) + prm[2:]
         if prm[0] == 'encoder':
 
             if prm[1] == 'mid_block':
@@ -28,6 +35,8 @@ def convert_vae_weights(vae_params_flatten, debug: bool = False):
                         prm = prm[:3] + ('c1',) + prm[4:]
                     if prm[3] == 'conv2':
                         prm = prm[:3] + ('c2',) + prm[4:]
+                    if prm[3] == 'conv_shortcut':
+                        prm = prm[:3] + ('cs',) + prm[4:]
                 prm = ('encoder', 'bottle_neck') + prm[2:]
 
             if prm[1].startswith('down_blocks_'):
@@ -37,8 +46,12 @@ def convert_vae_weights(vae_params_flatten, debug: bool = False):
                         prm = prm[:3] + ('c1',) + prm[4:]
                     if prm[3] == 'conv2':
                         prm = prm[:3] + ('c2',) + prm[4:]
+                    if prm[3] == 'conv_shortcut':
+                        prm = prm[:3] + ('cs',) + prm[4:]
                 prm = ('encoder', f'encoders_{num}') + prm[2:]
+            if prm[1] == 'conv_norm_out':
+                prm = prm[0:1] + ('norm_out',) + prm[2:]
+        new_params[prm] = v
         if debug:
             print(f'{org_prm} -> {prm}')
-        new_params[prm] = v
     return new_params
